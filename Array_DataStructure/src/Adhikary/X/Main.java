4fc71887be46389961bdd578abb9ae4845c585e6
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-07-04 10:31:35 +0530

    finding elements in array takes o(n)

--- Array_DataStructure/src/Adhikary/X/Main.java
+++ Array_DataStructure/src/Adhikary/X/Main.java
@@ -5,6 +5,22 @@ public class Main {
     public static void main(String... args)
     {
 
+        int[] intArray = {20,35,-15,7,55,1,-22};
+
+        int index = 0;
+        for(int i = 0 ; i <intArray.length ; i++)
+        {
+            if(intArray[i] == 7)
+            {
+//            System.out.println("index = " + i);
+            index = i ;
+                break;
+            }
+        }
+
+        System.out.println("index = " + index);
+
+
 
 
     }
